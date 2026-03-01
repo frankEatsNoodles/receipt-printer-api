8d6f0b4e610c1a8f5528d0b2590a931c14216433
frankEatsNoodles <55frankwu@gmail.com>
2026-02-28 23:52:50 -0500

    Deleted minor comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "api:app",  # Changed from "main:app" to "api:app"
+        "api:app",  
         host="127.0.0.1",
         port=8000,
         reload=True
